e3617d29867757a68d4a8564fded1d3daf651f14
liuhanzhi <liuhz0123@gmail.com>
2016-10-18 15:14:49 +0800

    test fragment

---
+++ testfragment/src/main/java/com/lhz/test/testfragment/MainActivity.java
@@ -0,0 +1,82 @@
+package com.lhz.test.testfragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+
+/**
+ * 结论:在activity中replace一个fragment操作,执行commit后,不会立即调用fragment的生命周期,即该fragment不是立即被添加到activity中来。
+ */
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    MyFragment myFragment;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
+
+    }
+
+    private void addFragment(){
+        RelativeLayout container = (RelativeLayout) findViewById(R.id.container);
+
+        myFragment = new MyFragment();
+        Log.e("lhz","init------textview is "+ (myFragment.getTextView() == null?"null":"not null"));
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.container,myFragment);
+        Log.e("lhz","replace------textview is "+ (myFragment.getTextView() == null?"null":"not null"));
+
+
+//        fragmentTransaction.commitAllowingStateLoss();
+        fragmentTransaction.commit();
+
+        Log.e("lhz","commit----textview is "+ (myFragment.getTextView() == null?"null":"not null"));
+
+        container.post(new Runnable() {
+            @Override
+            public void run() {
+                Log.e("lhz","post----textview is "+ (myFragment.getTextView() == null?"null":"not null"));
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        addFragment();
+    }
+
+    public static class MyFragment extends Fragment {
+
+        public TextView textView;
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            Log.e("lhz","onCreateView");
+            TextView textView = new TextView(getContext());
+            textView.setText("this is a fragment!");
+            this.textView = textView;
+            return textView;
+        }
+
+        public TextView getTextView(){
+            return textView;
+        }
+
+
+    }
+}
